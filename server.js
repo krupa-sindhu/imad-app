8b31254ed0e67faad0ad5cf2e13d05e4fdeb4599
krupa-sindhu <30946886+krupa-sindhu@users.noreply.github.com>
2017-08-21 20:53:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,9 +96,22 @@ app.get('/test-db',function(req,res){
          res.send(JSON.stringify(result.rows));
     });
 });
-app.get('/:articleName',function(req,res){
-      var  articleName = req.params.articleName;
-     res.send(createTemplate.articles[articleName]);
+app.get('/article/:articleName',function(req,res){
+      pool.query("SELECT * FROM TEST WHERE title" + req.params.articleName,function(err,result)
+      {if(err)
+         res.status(500).send(err.toString());
+       else{
+           if(result.rows.length===0){
+               res.status(404).send('article not found');
+            }
+       else 
+       var articleData=result.rows[0];
+        res.send(createTemplate.articles[articleName]);
+      }
+      });
+      
+      
+    
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
