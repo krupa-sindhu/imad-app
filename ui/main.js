55785df9df444488f6d38d37651f01e911143af8
krupa-sindhu <30946886+krupa-sindhu@users.noreply.github.com>
2017-08-25 14:09:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,55 +1,28 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element;
-element.innerHTML='krupa!!!';
-var img=document.getElementById('madi');
-marginLeft=0;
-function moveRight(){
-    marginLeft+=10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
-   // img.style.marginLeft='100px';
-};
 
-var button=document.getElementById('counter');
-button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-    if(request.readyState==XMLHttpRequest.DONE){
-        if(request.status==200){
-        var ctr=request.responseText;
-      var span=document.getElementById('count');
-span.innerHTML=ctr.toString();  
-    }
-    }
-};
-request.open('GET','http://krps123450.imad.hasura-app.io/counter',true);
-request.send(null);
-};
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
  var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
     if(request.readyState==XMLHttpRequest.DONE){
         if(request.status==200){
-         var names=request.responseText;
-         names=JSON.parse(names);
-   var list='';
-   for(var i=0;i<names.length;i++)
-   {
-   list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list;
+        console.log('user logged in');
+        alert('logged in sucessfully');
+    }else if(request.status===403){
+       alert('password is incorrect');
+    }else if(request.status===500){
+        alert('something went wrong on the server');
     }
     }
     };
-    var nameInput=document.getElementById('name');
+    var username=document.getElementById('username').value;
+    var password =document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     var name=nameInput.value;
-    request.open('GET','http://krps123450.imad.hasura-app.io/submit-name?name='+name,true);
-request.send(null);
+    
+    request.open('POST','http://krps123450.imad.hasura-app.io/login',true);
+    request.setRequestHeader('content-type','application/json');
+    request.send(JSON.stringify({username:username,password:password}));
 
   
 };
